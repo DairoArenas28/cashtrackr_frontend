4da41e997a2efc1a1fcd497f887774c7fd44b576
Dairo Arenas <dairo.arenas802@pascualbravo.edu.co>
2025-05-02 15:43:47 -0500

    cambio

--- app/admin/profile/password/page.tsx
+++ app/admin/profile/password/page.tsx
@@ -5,7 +5,7 @@ export default async function ChangePasswordPage() {
         <>
             <h1 className="font-black text-4xl text-purple-950 my-5">Cambiar Password</h1>
             <p className="text-xl font-bold">Aquí puedes cambiar tu {''}
-                <span className="text-amber-500">password</span>
+                <span className="text-amber-500">password </span>
             </p>
 
             <ChangePasswordForm/>
